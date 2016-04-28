b8d19222f962850eb3f0aa4c6f4b3eff95ba4c5f
Jerico <jerico.indigo@gmail.com>
2016-04-28 20:10:13 +0800

    With Model

--- app/App.php
+++ app/App.php
@@ -1,9 +1,15 @@
 <?php
 
 namespace Liquido;
+use Liquido\Model as Model;
 
 class App{
-	public function index(){
+	public static function index(){
         return 'Hello World, Composer!';
+    }
+    public static function add($num1, $num2){
+    	$model = new Model\BaseModel;
+    	$output = $model->add($num1, $num2);
+    	return $output;
     }
 }
---
+++ app/Model/BaseModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Liquido\Model;
+
+class BaseModel{
+	public function add($num1, $num2){
+		return $num1+$num2;
+	}
+}
